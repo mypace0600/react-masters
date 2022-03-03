db2257b02f32c6331efd71fc403ef5474cd3dd7f
mypace0600 <mypace0600@gmail.com>
2022-03-03 22:40:18 +0900

    3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
 import React, {useState} from "react";
-import Circle from "./Circle";
+import styled from "styled-components";
+
+const Container = styled.div`
+background-color:${(props)=>props.theme.bgColor};
+`;
+const H1=styled.h1`
+color:${(props)=>props.theme.textColor};
+`;
 
 function App() {
-  const [name, setName]= useState("");
-  const onChange =(event:React.FormEvent<HTMLInputElement>)=>{
-    const {
-      currentTarget:{value},
-    }= event;
-    setName(value);
-  };
   
-  const onSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
-    console.log("hello", name);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input 
-          value={name} 
-          onChange={onChange} 
-          type="text" 
-          placeholder="username"
-        />
-        <button>
-          Log in
-        </button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hello~</H1>
+    </Container>
   );
 }
 
