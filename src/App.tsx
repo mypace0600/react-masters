670e943b1bfaba33e61ce0639026b1a8354e892f
mypace0600 <mypace0600@gmail.com>
2022-03-03 21:56:12 +0900

    3.3 Optional Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import styled,{keyframes} from "styled-components"
-
-
-const Title = styled.h1`
-  color: ${(props)=>props.theme.textColor}
-`;
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  display:flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props)=>props.theme.backgroundColor};
-`;
+import Circle from "./Circle";
 
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+    <div>
+      <Circle borderColor="blue" bgColor={"teal"}/>
+      <Circle text="good" bgColor={"tomato"}/>
+    </div>
   );
 }
 
