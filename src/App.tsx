1f299e9cc734ed9eb3781916906cf8c425302fcb
mypace0600 <mypace0600@gmail.com>
2022-03-03 22:23:42 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import React, {useState} from "react";
 import Circle from "./Circle";
 
-
 function App() {
+  const [name, setName]= useState("");
+  const onChange =(event:React.FormEvent<HTMLInputElement>)=>{
+    const {
+      currentTarget:{value},
+    }= event;
+    setName(value);
+  };
+  
+  const onSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    console.log("hello", name);
+  };
   return (
     <div>
-      <Circle borderColor="blue" bgColor={"teal"}/>
-      <Circle text="good" bgColor={"tomato"}/>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={name} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="username"
+        />
+        <button>
+          Log in
+        </button>
+      </form>
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,32 +1,15 @@
-import styled from "styled-components";
-import {useState} from "react";
+import styled from "styled-components"
 
-interface ContainerProps{
-    bgColor:string;
-    borderColor?:string;
-}
- 
-const Container = styled.div<ContainerProps>`
-    width: 200px;
-    height: 200px;
+
+const Container = styled.div`
+    width: 100px;
+    height: 100px;
     border-radius: 50%;
-    background-color: ${(props) =>props.bgColor};
-    border:1px solid ${(props)=>props.borderColor};
 `;
 
-interface CircleProps {
-    bgColor: string;
-    borderColor?:string;
-    text?:string;
-}
-
-function Circle({bgColor, borderColor, text="default text"}:CircleProps){
-    const [counter, setCounter] = useState(0);
-    setCounter(1)
+function Circle(){
     return(
-        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
-            {text}
-        </Container>
+        <Container />
     );
 }
 
